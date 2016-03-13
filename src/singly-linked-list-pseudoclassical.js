54f316fd4b8c666bc969990b4c51ddf94d0996cb
Tom O'Connell <tomoconnell@live.com>
2016-03-13 13:32:12 -0700

    Add tests for most methodss.

---
+++ src/singly-linked-list-pseudoclassical.js
@@ -0,0 +1,80 @@
+var LinkedList = function () {
+  this.head = null;
+  this.tail = null;
+  this.size = 0;
+};
+
+LinkedList.prototype.getSize = function () {
+  return this.size;
+};
+
+LinkedList.prototype.insertAtEnd = function (value) {
+  var newTail = {value: value, next: null};
+  if (!this.tail) {
+    this.tail = newTail;
+    this.head = newTail;
+  } else {
+    this.tail.next = newTail;
+    this.tail = newTail;
+  }
+  this.size++;
+};
+
+LinkedList.prototype.insertAtBeginning = function (value) {
+  var newHead = {value: value, next: null};
+  if (!this.head) {
+    this.head = newHead;
+    this.tail = newHead;
+  } else {
+    newHead.next = this.head;
+    this.head = newHead;
+  }
+  this.size++;
+};
+
+LinkedList.prototype.removeFromEnd = function () {
+  var temp;
+  var checkNode = function (node) {
+    var temp;
+    if (node.next && node.next.next === null) {
+      temp = node.next.value;
+      node.next = null;
+      this.tail = node;
+      return temp;
+    } else {
+      return checkNode(node.next);
+    }
+  };
+
+  if (this.size === 1) {
+    temp = this.tail.value;
+    this.tail = null;
+    this.head = null;
+  } else if (this.size === 2) {
+    temp = this.tail.value;
+    this.head.next = null;
+    this.tail = this.head;
+  } else if (this.size > 2) {
+    temp = checkNode(this.head);
+  }
+  if (this.size > 0) {
+    this.size--;
+  }
+  return temp;
+};
+
+LinkedList.prototype.contains = function (value) {
+  var checkNode = function (node) {
+    if (node.value === value) {
+      return true;
+    }
+    if (!node.next) {
+      return false;
+    }
+    return checkNode(node.next);
+  };
+
+  return checkNode(this.head);
+};
+
+module.exports = LinkedList;
---
+++ test/singly-linked-list-pseudoclassical.js
@@ -0,0 +1,57 @@
+var LinkedList = require('../src/singly-linked-list-pseudoclassical');
+var expect = require('chai').expect;
+
+describe('singly-linked-list-pseudoclassical', function () {
+  it('Should insert values at end.', function () {
+    var linkedList = new LinkedList();
+    linkedList.insertAtEnd(42);
+    expect(linkedList.getSize()).to.equal(1);
+    expect(linkedList.contains(42)).to.equal(true);
+  });
+
+  it('Should insert values at beginning.', function () {
+    var linkedList = new LinkedList();
+    linkedList.insertAtBeginning(42);
+    expect(linkedList.getSize()).to.equal(1);
+    expect(linkedList.contains(42)).to.equal(true);
+  });
+
+  it('Should remove values from end when size is 0.', function () {
+    var linkedList = new LinkedList();
+    expect(linkedList.getSize()).to.equal(0);
+    expect(linkedList.removeFromEnd()).to.equal(undefined);
+    expect(linkedList.getSize()).to.equal(0);
+  });
+
+  it('Should remove values from end when size is 1.', function () {
+    var linkedList = new LinkedList();
+    linkedList.insertAtEnd('a');
+    expect(linkedList.getSize()).to.equal(1);
+    expect(linkedList.removeFromEnd()).to.equal('a');
+    expect(linkedList.getSize()).to.equal(0);
+  });
+
+  it('Should remove values from end when size is 2.', function () {
+    var linkedList = new LinkedList();
+    linkedList.insertAtEnd(42);
+    linkedList.insertAtEnd('a');
+    expect(linkedList.getSize()).to.equal(2);
+    expect(linkedList.contains('a')).to.equal(true);
+    expect(linkedList.removeFromEnd()).to.equal('a');
+    expect(linkedList.getSize()).to.equal(1);
+    expect(linkedList.contains('a')).to.equal(false);
+  });
+
+  it('Should remove values from end when size is > 2.', function () {
+    var linkedList = new LinkedList();
+    linkedList.insertAtEnd(42);
+    linkedList.insertAtEnd('a');
+    linkedList.insertAtEnd('b');
+    expect(linkedList.getSize()).to.equal(3);
+    expect(linkedList.contains('b')).to.equal(true);
+    expect(linkedList.removeFromEnd()).to.equal('b');
+    expect(linkedList.getSize()).to.equal(2);
+    expect(linkedList.contains('b')).to.equal(false);
+    expect(linkedList.contains('a')).to.equal(true);
+  });
+});
