7698d5b603d127053ace71f3f392ca9a6a81e72f
Tom O'Connell <tomoconnell@live.com>
2016-04-11 17:34:09 -0700

    Add bst remove method.

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -38,6 +38,30 @@ BinarySearchTree.prototype._getNode = function (node, value) {
   }
 };
 
+BinarySearchTree.prototype._getParentNode = function (node, value) {
+  if (value < node.value) {
+    if (value === node.left.value) {
+      return node;
+    } else {
+      return this._getParentNode(node.left, value);
+    }
+  } else {
+    if (value === node.right.value) {
+      return node;
+    } else {
+      return this._getParentNode(node.right, value);
+    }
+  }
+};
+
+BinarySearchTree.prototype._getParent = function (value) {
+  if (this.count < 2 || this.root === null || this.root.value === value) {
+    return null;
+  } else {
+    return this._getParentNode(this.root, value);
+  }
+};
+
 BinarySearchTree.prototype.contains = function (value) {
   if (this._getNode(this.root, value) === null) {
     return false;
@@ -53,6 +77,103 @@ BinarySearchTree.prototype.insert = function (value) {
     this._addNode(this.root, value);
   }
   this.count++;
+};
+
+BinarySearchTree.prototype.remove = function (value) {
+  var nodeToRemove = this._getNode(this.root, value);
+  var parentNode;
+  var largestOnLeft;
+  var largestOnLeftParent;
+  var parentIsTheNodeToRemove;
+
+  if (nodeToRemove === null) {
+    return false;
+  }
+
+  if (this.count === 1) {
+    this.root = null;
+    this.count = 0;
+    return true;
+  }
+
+  parentNode = this._getParent(value);
+
+  // Covers edge case; removing the root and there is more than 1 node.
+  if (parentNode === null) {
+    if (this.root.left === null && this.root.right !== null) {
+      this.root = this.root.right;
+    } else if (this.root.left !== null && this.root.right === null) {
+      this.root = this.root.left;
+    } else {
+      largestOnLeft = this.root.left;
+      parentIsTheNodeToRemove = true;
+
+      while (largestOnLeft.right !== null) {
+        largestOnLeft = largestOnLeft.right;
+        parentIsTheNodeToRemove = false;
+      }
+
+      if (parentIsTheNodeToRemove === true) {
+        this.root.value = this.root.left.value;
+        this.root.left = null;
+      } else {
+        largestOnLeftParent = this._getParent(largestOnLeft.value);
+        this.root.value = largestOnLeft.value;
+
+        if (largestOnLeft.left !== null) {
+          largestOnLeftParent.right = largestOnLeft.left;
+        } else {
+          largestOnLeftParent.right = null;
+        }
+      }
+    }
+    this.count--;
+    return true;
+  }
+
+  if (nodeToRemove.left === null && nodeToRemove.right === null) {
+    if (nodeToRemove.value < parentNode.value) {
+      parentNode.left = null;
+    } else {
+      parentNode.right = null;
+    }
+  } else if (nodeToRemove.left !== null && nodeToRemove.right === null) {
+    if (nodeToRemove.value < parentNode.value) {
+      parentNode.left = nodeToRemove.left;
+    } else {
+      parentNode.right = nodeToRemove.left;
+    }
+  } else if (nodeToRemove.left === null && nodeToRemove.right !== null) {
+    if (nodeToRemove.value < parentNode.value) {
+      parentNode.left = nodeToRemove.right;
+    } else {
+      parentNode.right = nodeToRemove.right;
+    }
+  } else {
+    largestOnLeft = nodeToRemove.left;
+    parentIsTheNodeToRemove = true;
+
+    while (largestOnLeft.right !== null) {
+      largestOnLeft = largestOnLeft.right;
+      parentIsTheNodeToRemove = false;
+    }
+
+    if (parentIsTheNodeToRemove === true) {
+      nodeToRemove.value = nodeToRemove.left.value;
+      nodeToRemove.left = null;
+    } else {
+      largestOnLeftParent = this._getParent(largestOnLeft.value);
+      nodeToRemove.value = largestOnLeft.value;
+
+      if (largestOnLeft.left !== null) {
+        largestOnLeftParent.right = largestOnLeft.left;
+      } else {
+        largestOnLeftParent.right = null;
+      }
+    }
+  }
+  this.count--;
+  return true;
 };
 
 module.exports = BinarySearchTree;
--- test/binary-search-tree.js
+++ test/binary-search-tree.js
@@ -16,4 +16,24 @@ describe('binary-search-tree', function () {
     expect(bst.contains(900)).to.equal(true);
     expect(bst.count).to.equal(4);
   });
+
+  it('Should remove values.', function () {
+    var bst = new BinarySearchTree();
+
+    bst.insert(2);
+    bst.insert(1);
+    bst.insert(3);
+    bst.insert(900);
+
+    bst.remove(2);
+    bst.remove(1);
+    bst.remove(3);
+    bst.remove(900);
+
+    expect(bst.count).to.equal(0);
+    expect(bst.contains(2)).to.equal(false);
+    expect(bst.contains(1)).to.equal(false);
+    expect(bst.contains(3)).to.equal(false);
+    expect(bst.contains(900)).to.equal(false);
+  });
 });
