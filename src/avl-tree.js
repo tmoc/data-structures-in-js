501567b827db0ddd924b05b0a9981ecb9d92d87e
Tom O'Connell <codertomo@gmail.com>
2016-04-14 09:02:36 -0700

    Add rebalancing methods.

--- src/avl-tree.js
+++ src/avl-tree.js
@@ -26,6 +26,32 @@ AVLTree.prototype._makeNode = function (value) {
   return {value: value, height: 0, left: null, right: null};
 };
 
+AVLTree.prototype._rightRotation = function (node) {
+  var oldLeft = node.left;
+  node.left = oldLeft.right;
+  oldLeft.right = node;
+  node.height = this._maxHeight(node.left, node.right) + 1;
+  oldLeft.height = this._maxHeight(oldLeft.left, oldLeft.right) + 1;
+};
+
+AVLTree.prototype._leftRotation = function (node) {
+  var oldRight = node.right;
+  node.right = oldRight.left;
+  oldRight.left = node;
+  node.height = this._maxHeight(node.left, node.right) + 1;
+  oldRight.height = this._maxHeight(oldRight.left, oldRight.right) + 1;
+};
+
+AVLTree.prototype._rightAndLeftRotation = function (node) {
+  this._rightRotation(node.right);
+  this._leftRotation(node);
+};
+
+AVLTree.prototype._leftAndRightRotation = function (node) {
+  this._leftRotation(node.left);
+  this._rightRotation(node);
+};
+
 AVLTree.prototype._balanceFactor = function (leftNode, rightNode) {
   var leftNodeHeight = leftNode !== null ? leftNode.height : -1;
   var rightNodeHeight = rightNode !== null ? rightNode.height : -1;
@@ -41,16 +67,16 @@ AVLTree.prototype._maxHeight = function (leftNode, rightNode) {
 AVLTree.prototype.checkBalance = function (node) {
   if (this._balanceFactor(node.left, node.right) === 2) { // Left too high.
     if (this._balanceFactor(node.left.left, node.left.right) > 0) {
-      // Right rotation.
+      this._rightRotation(node);
     } else {
-      // Left and right rotation.
+      this._leftAndRightRotation(node);
     }
 
   } else if (this._balanceFactor(node.left, node.right) === -2) { // Right too high.
     if (this._balanceFactor(node.right.left, node.right.right) < 0) {
-      // Left rotation.
+      this._leftRotation(node);
     } else {
-      // Right and left rotation.
+      this._rightAndLeftRotation(node);
     }
   } else {
     node.height = this._maxHeight(node.left, node.right) + 1;
